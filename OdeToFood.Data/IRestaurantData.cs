00ec01f986c772f63499d3c2197f9da20dc307ac
Luke D Buchanan <luke.d.buchanan@pwc.com>
2022-01-28 12:33:21 +0000

    Initial Commit

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,64 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        int Commit();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+            new Restaurant { Id = 1, Name = "Luke's Pizza", Location = "Ballymena", Cuisine = CuisineType.Italian },
+            new Restaurant { Id = 2, Name = "Boojum", Location = "Belfast", Cuisine = CuisineType.Mexican },
+            new Restaurant { Id = 3, Name = "Khayber", Location = "Galgorm", Cuisine = CuisineType.Indian }
+            };
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+          return from r in restaurants
+                 where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                 orderby r.Name
+                 select r;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+
+            if(restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+    }
+
+}
